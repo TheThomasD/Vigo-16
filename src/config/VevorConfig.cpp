e65e11911d58b020a6fbe17ed4d4d481abf7b645
TheThomasD <35131193+TheThomasD@users.noreply.github.com>
2023-09-24 23:37:41 +0200

    added config for baud rate and feed rate

--- src/config/VevorConfig.cpp
+++ src/config/VevorConfig.cpp
@@ -8,6 +8,8 @@ void VevorConfig::load()
     staSsid = prefs.getString(nameStaSsid, "");
     staPassword = prefs.getString(nameStaPassword, "");
     hostName = prefs.getString(nameHostName, "Vevor-CNC");
+    baudRate = fromValue(prefs.getULong(nameBaudRate, 115200));
+    feedRate = prefs.getUShort(nameFeedRate, 1000);
     prefs.end();
 }
 
@@ -19,6 +21,8 @@ void VevorConfig::save()
     prefs.putString(nameStaSsid, staSsid);
     prefs.putString(nameStaPassword, staPassword);
     prefs.putString(nameHostName, hostName);
+    prefs.putULong(nameBaudRate, toValue(baudRate));
+    prefs.putUShort(nameFeedRate, feedRate);
     prefs.end();
 }
 
@@ -36,6 +40,26 @@ void VevorConfig::print()
     printPair(nameStaSsid, staSsid);
     printPair(nameStaPassword, staPassword);
     printPair(nameHostName, hostName);
+    printPair(nameBaudRate, String(toValue(baudRate)));
+    printPair(nameFeedRate, String(feedRate));
+}
+
+uint32_t VevorConfig::toValue(BaudRate rate)
+{
+#define COMMAND_BAUD_RATE_TOVALUE(RATE) \
+    if (rate == BR_##RATE)              \
+        return RATE;
+    FOREACH_BAUD_RATE(COMMAND_BAUD_RATE_TOVALUE)
+    return 115200;
+}
+
+VevorConfig::BaudRate VevorConfig::fromValue(uint32_t value)
+{
+#define COMMAND_BAUD_RATE_FROMVALUE(RATE) \
+    if (value == RATE)                    \
+        return BR_##RATE;
+    FOREACH_BAUD_RATE(COMMAND_BAUD_RATE_FROMVALUE)
+    return BaudRate::BR_115200;
 }
 
 void VevorConfig::setApSsid(String ssid)
@@ -86,4 +110,24 @@ void VevorConfig::setHostName(String name)
 String VevorConfig::getHostName()
 {
     return hostName;
+}
+
+void VevorConfig::setBaudRate(BaudRate rate)
+{
+    baudRate = rate;
+}
+
+VevorConfig::BaudRate VevorConfig::getBaudRate()
+{
+    return baudRate;
+}
+
+void VevorConfig::setFeedRate(uint16_t rate)
+{
+    feedRate = rate;
+}
+
+uint16_t VevorConfig::getFeedRate()
+{
+    return feedRate;
 }
--- src/config/VevorConfig.h
+++ src/config/VevorConfig.h
@@ -6,6 +6,24 @@
 class VevorConfig
 {
 public:
+#define FOREACH_BAUD_RATE(COMMAND) \
+    COMMAND(9600)                  \
+    COMMAND(19200)                 \
+    COMMAND(28800)                 \
+    COMMAND(38400)                 \
+    COMMAND(57600)                 \
+    COMMAND(76800)                 \
+    COMMAND(115200)                \
+    COMMAND(230400)                \
+    COMMAND(460800)                \
+    COMMAND(576000)                \
+    COMMAND(921600)
+#define COMMAND_BAUD_RATE_ENUM(RATE) BR_##RATE,
+    enum BaudRate
+    {
+        FOREACH_BAUD_RATE(COMMAND_BAUD_RATE_ENUM)
+    };
+
     void load();
     void save();
     void print();
@@ -20,11 +38,17 @@ public:
     String getStaPassword();
     void setHostName(String hostname);
     String getHostName();
+    void setBaudRate(BaudRate baudRate);
+    BaudRate getBaudRate();
+    uint32_t toValue(BaudRate rate);
+    void setFeedRate(uint16_t feedRate);
+    uint16_t getFeedRate();
 
 protected:
     Preferences prefs;
 
     void printPair(const char *name, String value);
+    BaudRate fromValue(uint32_t value);
 
 private:
     String apSsid;
@@ -32,6 +56,8 @@ private:
     String staSsid;
     String staPassword;
     String hostName;
+    BaudRate baudRate;
+    uint16_t feedRate;
 
     const char *storageNamespace = "vigo-16";
     const char *nameApSsid = "apSsid";
@@ -39,4 +65,6 @@ private:
     const char *nameStaSsid = "staSsid";
     const char *nameStaPassword = "staPw";
     const char *nameHostName = "host";
+    const char *nameBaudRate = "bRate";
+    const char *nameFeedRate = "fRate";
 };
