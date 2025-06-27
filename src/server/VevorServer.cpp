4069177cf3b854ab859d5c7e7721bfcd64038be4
regev tal snijder <regev@devpond.nl>
2025-06-27 12:30:20 +0200

    implemented file manager tab

--- src/server/VevorServer.cpp
+++ src/server/VevorServer.cpp
@@ -1,28 +1,138 @@
 #include "VevorServer.h"
 #include <SPIFFS.h>
 #include <ElegantOTA.h>
+#include <SD.h>
+#include "../sd/SdCard.h"
 #include "../log/Logger.h"
 
 void VevorServer::init(AsyncWebServer *server, AsyncWebSocket *webSocket, WebSocketHandler *handler)
 {
-    if (SPIFFS.begin(true))
-        log_println("SPIFFS mounted.");
-    else
-        log_println("SPIFFS could not be mounted!");
+  if (SPIFFS.begin(true))
+    log_println("SPIFFS mounted.");
+  else
+    log_println("SPIFFS could not be mounted!");
 
-    server->onNotFound([](AsyncWebServerRequest *request)
-                       { request->send(404); });
+  server->onNotFound([](AsyncWebServerRequest *request)
+                     { request->send(404); });
 
-    server->on("/", HTTP_GET, [](AsyncWebServerRequest *request)
-               { request->redirect("/index.html"); });
+  server->on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+             { request->redirect("/index.html"); });
 
-    server->serveStatic("/", SPIFFS, "/");
+  server->on("/upload", HTTP_POST, [](AsyncWebServerRequest *request)
+             { request->send(200, "text/plain", "File uploaded successfully"); }, [](AsyncWebServerRequest *request, String filename, size_t index, uint8_t *data, size_t len, bool final)
+             {
+               SdCard *sdCard = new SdCard();
+               sdCard->mount();
+               if (!sdCard->isMounted())
+               {
+                 log_println("SD card not mounted!");
+                 return;
+               }
+               bool isMounted = sdCard->ensureMounted();
+               if (isMounted)
+               {
 
-    webSocket->onEvent([handler](AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len)
-                       { handler->onEvent(server, client, type, arg, data, len); });
-    server->addHandler(webSocket);
+                 if (!index)
+                 {
+                   Serial.printf("UploadStart: %s\n", filename.c_str());
+                   // Open the file for writing
+                   request->_tempFile = SD.open("/" + filename, FILE_WRITE);
+                 }
+                 if (request->_tempFile)
+                 {
+                   request->_tempFile.write(data, len); // Write chunk
+                 }
+                 if (final)
+                 {
+                   if (request->_tempFile)
+                   {
+                     request->_tempFile.close();
+                   }
+                   Serial.printf("UploadEnd: %s (%u bytes)\n", filename.c_str(), index + len);
+                 }
+                 sdCard->unmount();
+               } });
 
-    ElegantOTA.begin(server);
+  server->on("/delete", HTTP_GET, [](AsyncWebServerRequest *request)
+             {
+               log_println("delete file");
+               SdCard *sdCard = new SdCard();
+               sdCard->mount();
+               if (!sdCard->isMounted())
+               {
+                 log_println("SD card not mounted!");
+                 request->send(500, "text/plain", "SD card not mounted!");
 
-    server->begin();
+                 return;
+               }
+               bool isMounted = sdCard->ensureMounted();
+               if (isMounted)
+               {
+                 String path = request->getParam("file")->value();
+                 log_println("path: " + path);
+                 if (!SD.exists(path))
+                 {
+                   request->send(404, "text/plain", "File not found");
+                   return;
+                 }
+                 if (SD.remove(path))
+                 {
+                   Serial.println("Deleted: " + path);
+                   request->send(200);
+                 }
+                 else
+                 {
+                   request->send(500, "text/plain", "Failed to delete file");
+                 }
+                 sdCard->unmount();
+                 return;
+               }
+               request->send(500, "text/plain", "SD card not mounted!"); });
+
+  server->on("/download", HTTP_GET, [](AsyncWebServerRequest *request)
+             {
+                log_println("download");
+
+                if (!request->hasParam("file")) {
+                  request->send(400, "text/plain", "Missing 'file' parameter");
+                  return;
+                }
+                SdCard *sdCard = new SdCard();
+                sdCard->mount();
+                if (!sdCard->isMounted())
+                {
+                  log_println("SD card not mounted!");
+                  return;
+                }
+                bool isMounted = sdCard->ensureMounted();
+                if (isMounted)
+                {
+                  String path = request->getParam("file")->value();
+
+                  if (!SD.exists(path)) {
+                    request->send(404, "text/plain", "File not found");
+                    return;
+                  }
+                  
+                  log_println("Downloading file...");
+                  request->send(SD, path, String(), true);
+                  TimerHandle_t t = xTimerCreate("sdunmnt", pdMS_TO_TICKS(2000), pdFALSE, nullptr, [](TimerHandle_t) {
+                    log_println("unmounting sd card");
+                      SdCard sd;
+                      sd.unmount();
+                  });
+                  xTimerStart(t, 0);
+                  // log_println("unmounting sd card");
+                  // sdCard->unmount();
+              } });
+
+  server->serveStatic("/", SPIFFS, "/");
+
+  webSocket->onEvent([handler](AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len)
+                     { handler->onEvent(server, client, type, arg, data, len); });
+  server->addHandler(webSocket);
+
+  ElegantOTA.begin(server);
+
+  server->begin();
 }
--- src/server/WebSocketHandler.cpp
+++ src/server/WebSocketHandler.cpp
@@ -1,6 +1,8 @@
 #include "WebSocketHandler.h"
 #include "../log/Logger.h"
-
+#include "../sd/SdCard.h"
+#include <SD.h>
+#include <ArduinoJson.h>
 void WebSocketHandler::init(GrblSender *grblSender)
 {
     this->grblSender = grblSender;
@@ -30,6 +32,74 @@ void WebSocketHandler::onEvent(AsyncWebSocket *server, AsyncWebSocketClient *cli
 #ifdef DEBUG
         log_print("WS received data: ");
 #endif
+
+        if (data[0] == '{' && data[len - 1] == '}')
+        {
+            log_println("JSON data received!");
+            JsonDocument doc;
+            DeserializationError error = deserializeJson(doc, data);
+            if (error)
+            {
+                Serial.print(F("deserializeJson() failed: "));
+                Serial.println(error.f_str());
+                return;
+            }
+
+            String type = doc["type"].as<String>();
+            if (type == "files")
+            {
+
+                File currentPath = File();
+                File currentFile = File();
+                long currentPathPosition = 0;
+                SdCard *sdCard = new SdCard();
+                sdCard->mount();
+                if (!sdCard->isMounted())
+                {
+                    log_println("SD card not mounted!");
+                    return;
+                }
+                bool canShow = sdCard->ensureMounted();
+                if (canShow)
+                {
+                    currentPath = sdCard->getRoot();
+                    currentPath.rewindDirectory();
+
+                    for (int i = 0; i < currentPathPosition; i++)
+                        currentPath.openNextFile().close();
+                    currentFile.close();
+                    currentFile = File();
+
+                    JsonDocument responseDoc;
+                    responseDoc["type"] = "files";
+                    JsonArray filesArray = responseDoc.createNestedArray("files");
+                    for (int i = 0; true; i++)
+                    {
+                        File file = currentPath.openNextFile();
+                        if (file)
+                        {
+                            JsonObject fileObj = filesArray.createNestedObject();
+                            fileObj["name"] = file.name();
+                            fileObj["path"] = file.path();
+
+                            if (!currentFile)
+                                currentFile = file;
+                            else
+                                file.close();
+                        }
+                        else
+                            break;
+                    }
+
+                    String jsonResponse;
+                    serializeJson(responseDoc, jsonResponse);
+                    client->text(jsonResponse);
+
+                    sdCard->unmount();
+                }
+                return;
+            }
+        }
         data[len] = 0;
         log_println((char *)data);
         grblSender->sendGcode((char *)data);
